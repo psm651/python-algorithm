69da71d06ebcfb64b5faf4648b0fbfd0e0d5d416
psm651 <psm651@naver.com>
2021-02-17 23:04:44 +0900

    백준 구현중

---
+++ baekjoon2961.py
@@ -0,0 +1,11 @@
+import sys
+
+N = int(sys.stdin.readline())
+ingredient_list = []
+for i in range(N):
+    ingredient_list.append(map(int,sys.stdin.readline().split(' '))) 
+print ingredient_list
+
+for j in range()
+
+#print S, B
